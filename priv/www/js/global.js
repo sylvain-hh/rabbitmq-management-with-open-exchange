933c63660f8276597890530f691f77cf546e6f9a
sylvainhubsch <sylvain.hubsch@gmail.com>
2018-12-08 07:38:51 -0800

    Introduce new x-open exchange helpers

--- priv/www/js/global.js
+++ priv/www/js/global.js
@@ -522,8 +522,71 @@ var HELP = {
         <dd>Rate at which queues are created. Declaring a queue that already exists counts for a "Declared" event, but not for a "Created" event. </dd>\
         <dt>Deleted</dt>\
         <dd>Rate at which queues are deleted.</dd>\
-     </dl> '
+     </dl> ',
 
+    'x-match':
+    'The binding type determines how match operators are evaluated to decide if binding is true for a given message.\
+      <dl>\
+        <dt>all</dt>\
+        <dd>all match operators must be true (and all header keys must match in the message)</dd>\
+        <dt>any</dt>\
+        <dd>at least one match operator must be true</dd>\
+      </dl>',
+
+    'x-?hkv' :
+    'Compare values of a given key according to a given operator; for binary operators, the left term is defined by the message key\'s value. Match operator is evaluated as false if there is no corresponding key in message headers.\
+      <dl>\
+        <dt>&le; / &lt; / &gt; / &ge;</dt>\
+        <dd>numeric values only</dd>\
+        <dt>= / &#8800;</dt>\
+        <dd>depending on binding type, may be a list of values</dd>\
+        <dt>RE / !RE</dt>\
+        <dd>message\'s key value must match (NOT match) the regex defined in binding</dd>\
+      </dl>',
+
+    'x-?hk' :
+    'Base the match on the existence or the non-existence of some key(s) in message\'s headers. Value type is always string; may be a list of values.',
+
+    'x-?rk' :
+    'Compare values of message\'s routing key; value type is always string.\
+      <dl>\
+        <dt>= / &#8800;</dt>\
+        <dd>depending on binding type, may be a list of values</dd>\
+        <dt>RE / !RE</dt>\
+        <dd>message\'s routing key value must match (NOT match) the regex defined in binding</dd>\
+        <dt>AMQP topic / (ci)</dt>\
+        <dd>value is an AMQP topic pattern (* is one word, # is 0 or more words); use "(ci)" to be case insensitive</dd>\
+      </dl>',
+
+    'x-?dt' :
+    'REGEX match on the UTC or local datetime the message is processed by the exchange; value is evaluated ONCE per exchange entry.<br/><br/>The string format of the datetime is : "YYYYMMDD WW DW HHmmSS" (3 spaces) with :<br/>YYYY MM DD : year (4 digits) month (01..12) day (01..31)<br/>WW : the week number defined by ISO 8601 (01..53)<br/>DW : day of the week (1 for monday.. 7 is sunday)<br/>HH mm SS : hour (00..23) minute (00..59) second (00..59).',
+
+    'x-maindest':
+    'Change value of the primary destination.\
+      <dl>\
+        <dt>queue from msg rk</dt>\
+        <dd>the message routing key represents the destination queue, exactly like in default exchange</dd>\
+        <dt>delete</dt>\
+        <dd>as you can guess, remove primary destination (useful when destinations are otherwise defined for example)</dd>\
+      </dl>',
+
+    'x-order':
+    'Define the evaluation order of binding as an integer between 2000 and 15999; default is 9000. Other values are reserved for future use.',
+
+    'x-goto':
+    'Define the next binding to evaluate according to its order and depending on the current binding\'s evaluation; can only go formard and must be an integer between 2000 and 15999.',
+
+    'x-stop':
+    'Stop evaluate next bindings for that exchange depending on the current binding\'s evaluation.',
+
+    'x-routing' :
+    'Depending on the current binding\'s evaluation, allows to add or delete other destinations (queue(s) and/or exchange(s)). Contrary to the main destination declared in the binding, those other destinations are not validated so that they may exist or not, and binding is not deleted when they are deleted.\
+    <dl>\
+      <dt>true/false RE !RE</dt>\
+      <dd>use "true" or "false" to specify fully named destination, "RE" or "!RE" to regex match destinations\</dd>\
+      <dt>(m)</dt>\
+      <dd>allow those destinations to be set by message\'s header\
+    </dl>'
 };
 
 ///////////////////////////////////////////////////////////////////////////
